92f2cd46c11bb8cb0f770a5d3311bbc6935802ab
Antonio Brandao <antoniobrandaodesign@gmail.com>
2014-08-22 16:16:00 +0100

    Fixed JS task

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,25 +1,30 @@
-var gulp         = require('gulp');
-var	browserify 	 = require('browserify');
-var	source 		   = require('vinyl-source-stream');
-var	browserify 	 = require('browserify');
-var	gulpif 		   = require('gulp-if');
-var connect 	   = require('gulp-connect');
-var	streamify    = require('gulp-streamify');
-var	uglify 		   = require('gulp-uglify');
+var gulp          = require('gulp');
+var	browserify 	  = require('browserify');
+var	source 		    = require('vinyl-source-stream');
+var	browserify 	  = require('browserify');
+var	gulpif 		    = require('gulp-if');
+var connect 	    = require('gulp-connect');
+var	streamify     = require('gulp-streamify');
+var	uglify 		    = require('gulp-uglify');
 
-var watchify     = require('watchify');
-var bundleLogger = require('../util/bundleLogger');
-var handleErrors = require('../util/handleErrors');
-var striplog     = require('gulp-strip-debug');
+var watchify      = require('watchify');
+var bundleLogger  = require('../util/bundleLogger');
+var handleErrors  = require('../util/handleErrors');
+var strip         = require('gulp-strip-debug');
+var print         = require("gulp-print");
+
 
 gulp.task('js', ['environmentCheck'], function() {
 
-  var bundler = browserify({
+  var bundleMethod = global.isWatching ? watchify : browserify;
+
+  var bundler = bundleMethod({
     // Required watchify args
     cache: {}, packageCache: {}, fullPaths: true,
     // Browserify Options
     entries:    ['./src/js/main.js'],
     extensions: ['.coffee', '.hbs'],
+    // activate debug / source maps in development mode only
     debug:      global.ENV === 'development'
   });
 
@@ -29,7 +34,9 @@ gulp.task('js', ['environmentCheck'], function() {
     .bundle()
     .on('error', handleErrors)
     .pipe(source('bundle.js'))
-    .pipe(striplog())
+    // remove console.logs and such
+    .pipe(gulpif( global.ENV === 'production', streamify( strip() )))
+    // uglify JS and obfuscate in produciton mode only
     .pipe(gulpif( global.ENV === 'production', streamify(uglify({ mangle: global.ENV === 'production' }))))
     .pipe(gulp.dest(global.outputDir + global.dataPath + '/js'))
     .pipe(connect.reload())
@@ -37,7 +44,7 @@ gulp.task('js', ['environmentCheck'], function() {
   };
 
   if(global.isWatching) {
-    bundler = watchify(bundler);
+    // Rebundle with watchify on changes.
     bundler.on('update', bundle);
   }
 
