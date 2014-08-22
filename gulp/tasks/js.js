f64456ceca2bbde107675b8c6fee7a355e998a16
Antonio Brandao <antoniobrandaodesign@gmail.com>
2014-08-22 17:15:40 +0100

    Fix JS task

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -13,38 +13,36 @@ var handleErrors  = require('../util/handleErrors');
 var strip         = require('gulp-strip-debug');
 var print         = require("gulp-print");
 
+gulp.task('js', function() {
 
-gulp.task('js', ['environmentCheck'], function() {
-
-  var bundleMethod = global.isWatching ? watchify : browserify;
-
-  var bundler = bundleMethod({
+  var bundler = browserify({
     // Required watchify args
     cache: {}, packageCache: {}, fullPaths: true,
     // Browserify Options
     entries:    ['./src/js/main.js'],
     extensions: ['.coffee', '.hbs'],
-    // activate debug / source maps in development mode only
     debug:      global.ENV === 'development'
   });
 
   var bundle = function() 
   {
+    bundleLogger.start();
+
     return bundler
-    .bundle()
-    .on('error', handleErrors)
-    .pipe(source('bundle.js'))
-    // remove console.logs and such
-    .pipe(gulpif( global.ENV === 'production', streamify( strip() )))
-    // uglify JS and obfuscate in produciton mode only
-    .pipe(gulpif( global.ENV === 'production', streamify(uglify({ mangle: global.ENV === 'production' }))))
-    .pipe(gulp.dest(global.outputDir + global.dataPath + '/js'))
-    .pipe(connect.reload())
-    .on('end', bundleLogger.end);
+      .bundle()
+      .on('error', handleErrors)
+      .pipe(source('bundle.js'))
+      // remove console.logs and such
+      .pipe(gulpif( global.ENV === 'production', streamify( strip() )))
+      // uglify JS and obfuscate in produciton mode only
+      .pipe(gulpif( global.ENV === 'production', streamify(uglify({ mangle: global.ENV === 'production' }))))
+      .pipe(gulp.dest(global.outputDir + global.dataPath + '/js'))
+      .pipe(connect.reload())
+      .on('end', bundleLogger.end);
   };
 
   if(global.isWatching) {
-    // Rebundle with watchify on changes.
+    bundler = watchify(bundler);
     bundler.on('update', bundle);
   }
 
