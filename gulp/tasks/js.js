093128f6b70010d79d5a78fd5da8fa823b0bed79
Antonio Brandao <antoniobrandaodesign@gmail.com>
2015-02-03 13:19:52 +0000

    Updated JS task to have source maps

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -5,7 +5,7 @@ var	browserify 	  = require('browserify');
 var	gulpif 		    = require('gulp-if');
 var connect 	    = require('gulp-connect');
 var	streamify     = require('gulp-streamify');
-var	uglify 		    = require('gulp-uglify');
+var uglify        = require('gulp-uglify');
 
 var watchify      = require('watchify');
 var bundleLogger  = require('../util/bundleLogger');
@@ -21,46 +21,51 @@ var axis          = require("axis-css");
 var typographic   = require("typographic");
 var stylify       = require("stylify");
 
-var browserify_instance = browserify({
-  // Required watchify args
-  cache: {}, packageCache: {}, fullPaths: true,
-  // Browserify Options
-  entries:    ['./core/js/core.js'],
-  // extensions: ['.jade', '.styl'],
-  debug:      global.ENV === 'development'
-});
 
-browserify_instance.transform('stylify', {
-  use :[ 
-    jeet(),
-    rupture(), 
-    typographic(), 
-    axis(), 
-    autoprefixer({ browsers: ['ie 7', 'ie 8'] })
-  ],
-  sourcemap: { inline: global.ENV === 'development' },
-  compress: global.ENV === 'production',
-});
-
-var bundler = watchify(browserify_instance);
-
-var bundle = function()
+gulp.task('js_watch', ['environmentCheck'], function()
 {
-  return bundler.bundle()
-    .on('error', handleErrors)
-    .pipe(source('bundle.js'))
-    // remove console.logs and such
-    .pipe(gulpif( global.ENV === 'production', streamify( strip() )))
-    // uglify JS and obfuscate in produciton mode only
-    .pipe(gulpif( global.ENV === 'production', streamify(uglify({ mangle: global.ENV === 'production' }))))
-    .pipe(print())
-    .pipe(gulp.dest(global.outputDir + datapaths.dataPath + '/js'))
-    // .pipe(connect.reload())
-    .on('end', bundleLogger.end);
-}
+  var bundle = function()
+  {
+    return bundler.bundle()
+      .on('error', handleErrors)
+      .pipe(source('bundle.js'))
+      // remove console.logs and such
+      .pipe(gulpif( global.ENV === 'production', streamify( strip() )))
+      // uglify JS and obfuscate in produciton mode only
+      .pipe(gulpif( global.ENV === 'production', streamify(uglify({ mangle: global.ENV === 'production' }))))
+      .pipe(print())
+      .pipe(gulp.dest(global.outputDir + datapaths.dataPath + '/js'))
+      // .pipe(connect.reload())
+      .on('end', bundleLogger.end);
+  }
 
-gulp.task('js_watch', ['environmentCheck'], bundle); // so you can run `gulp js` to build the file
-bundler.on('update', bundle); // on any dep update, runs the bundler
+  var browserify_instance = browserify({
+    // Required watchify args
+    cache: {}, packageCache: {}, fullPaths: true,
+    // Browserify Options
+    entries:    ['./core/js/core.js'],
+    // extensions: ['.jade', '.styl'],
+    debug:      global.ENV === 'development'
+  });
+
+  browserify_instance.transform('stylify', {
+    use :[ 
+      jeet(),
+      rupture(), 
+      typographic(), 
+      axis(), 
+      autoprefixer({ browsers: ['ie 7', 'ie 8'] })
+    ],
+    sourcemap: { inline: global.ENV === 'development' },
+    compress: global.ENV === 'production',
+  });
+
+  var bundler = watchify(browserify_instance);
+  bundler.on('update', bundle); // on any dep update, runs the bundler
+
+  bundle();
+});
+
 
 
 
